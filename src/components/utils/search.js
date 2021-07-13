183c0d7a09db6b92edb4bc6aca6ca659b6e56490
rsands <d.russell.sands@gmail.com>
2021-07-13 16:19:35 -0400

    disabled search popup

--- src/components/utils/search.js
+++ src/components/utils/search.js
@@ -3,6 +3,7 @@ import Search from '@arcgis/core/widgets/Search';
 export const addSearchToView = (view) => {
   const search = new Search({
     view: view,
+    popupEnabled: false,
   });
   view.ui.add(search, {
     position: 'top-right',
