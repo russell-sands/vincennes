78901fcc48524688595489f6d6e6fe8161fbd923
rsands <d.russell.sands@gmail.com>
2021-07-14 13:58:08 -0400

    cleaned up no data messaging

--- src/components/DisplayNoData.js
+++ src/components/DisplayNoData.js
@@ -2,12 +2,27 @@ import React from 'react';
 import * as Constants from './Constants';
 
 export const DisplayNoData = ({ address }) => {
-  address = address ? address : 'this location';
   return (
-    <div className="result-header result-summary centered">
-      FEMA National Risk Index data are not available in {address}. For more
-      information about the National Risk Index, refer to{' '}
-      <a href={Constants.ABOUT_NRI_URL}>FEMA's documentation</a>.
+    <div className="result-header centered">
+      <div>
+        {(() => {
+          if (address) {
+            return (
+              <>
+                <span className="result-header-address">{`${address}`}</span>
+                <br />
+              </>
+            );
+          } else {
+            return <></>;
+          }
+        })()}
+        <span className="result-header-content">
+          FEMA National Risk Index data are not available for this location. For
+          more information about the National Risk Index, refer to{' '}
+          <a href={Constants.ABOUT_NRI_URL}>FEMA's documentation</a>.
+        </span>
+      </div>
     </div>
   );
 };
